f8454ea9d8c1326d9ce7afa1eebd592309799dab
Tyler-Burke2 <smilertyler@byui.edi>
2025-10-02 08:33:31 -0600

    finished prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What percentage is your grade?");
+        string grade = Console.ReadLine();
+
+
+        int number = int.Parse(grade);
+
+        if (number >= 90)
+        {
+            Console.WriteLine("Your letter grade is: A");
+            Console.WriteLine("Congrats on passing the class!");
+        }
+        else if (number >= 80)
+        {
+            Console.WriteLine("Your letter grade is: B");
+            Console.WriteLine("Congrats on passing the class!");
+        }
+        else if (number >= 70)
+        {
+            Console.WriteLine("Your letter grade is: C");
+            Console.WriteLine("Congrats on passing the class!");
+        }
+        else if (number >= 60)
+        {
+            Console.WriteLine("Your letter grade is: D");
+            Console.WriteLine("Do better next time!");
+        }
+        else if (number < 60)
+        {
+            Console.WriteLine("Your letter grade is: F");
+            Console.WriteLine("Do better next time!");
+        }
     }
 }
