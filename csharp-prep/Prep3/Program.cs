3ed98b9e63dae8058b36e1d8ca95b415c282e957
Tyler-Burke2 <smilertyler@byui.edi>
2025-10-02 08:55:13 -0600

    finished prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        int guesses;
+        do
+        {
+            Console.Write("What is your guess? ");
+            guesses = int.Parse(Console.ReadLine());
+
+            if (number > guesses)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (number < guesses)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        } while (number != guesses);
     }
 }
