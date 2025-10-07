2c522882ceb0ff535edb99aa9ab0fe5b420be4ff
Tyler-Burke2 <smilertyler@byui.edi>
2025-10-07 16:44:47 -0600

    finished prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Console.WriteLine("Welcome to the Program!");
+        Console.WriteLine("What is your full name?");
+        string name = Console.ReadLine();
+        Console.WriteLine("What is your favorite number? ");
+        string number = Console.ReadLine();
+        int Num = 0;
+        Num = int.Parse(number);
+        int square = Num * Num;
+        Console.WriteLine("What year were you born? ");
+        string birth = Console.ReadLine();
+        int Numb = int.Parse(birth);
+        int Age = 2025 - Numb;
+        Console.WriteLine($"{name}, the square of your number is: {square}");
+        Console.WriteLine($"{name}, you will turn {Age} this year.");
     }
 }
