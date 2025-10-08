3c12f99f89601f97c4c8ab9c5afa498b1871e149
Tyler-Burke2 <smilertyler@byui.edi>
2025-10-07 19:01:36 -0600

    finished learning 02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+
+        job1._company = "BYU-Idaho";
+        job1._jobTitle = "Ropes Course Facilitator";
+        job1._startYear = "2024";
+        job1._endYear = "2025";
+
+        job1.DisplayJobDetails();
+        // Console.WriteLine(job1._company);
+
+        Job job2 = new Job();
+
+        job2._company = "12 apostle dentistry";
+        job2._jobTitle = "Dentist";
+        job2._startYear = "1986";
+        job2._endYear = "2005";
+
+        job2.DisplayJobDetails();
+        // Console.WriteLine(job2._company);
+
+        Resume myResume = new Resume();
+        myResume._name = "Tyler Burke";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
