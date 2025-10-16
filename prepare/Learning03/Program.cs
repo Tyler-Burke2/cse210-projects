ec0c9767c5581e4a333ef5f713a5bd53a539feec
Tyler-Burke2 <smilertyler@byui.edi>
2025-10-15 18:23:56 -0600

    finshed learning 03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(6);
+        Fraction f3 = new Fraction(6, 7);
+        Fraction f4 = new Fraction(4, 8);
+
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f4.GetFractionString());
+
+
+        Console.WriteLine(f1.GetDecimalValue());
+        Console.WriteLine(f3.GetDecimalValue());
+        Console.WriteLine(f3.GetDecimalValue());
+        Console.WriteLine(f4.GetDecimalValue());
+
+
     }
 }
